8a775461c56834453c78a5f4cd463f54b6128faa
maryam0721 <you@example.com>
2022-12-18 05:37:56 -0800

    first_commit

--- bin/p1.dart
+++ bin/p1.dart
@@ -1,5 +1,74 @@
 import 'package:p1/p1.dart' as p1;
+import 'package:test/test.dart';
+
+enum MyEnum { buy, sell, outOfStore }
 
 void main(List<String> arguments) {
-  print('Hello world: ${p1.calculate()}!');
+  //----------------------------------------------------------------------------
+
+  // var a;
+  int? b;
+  print(b!);
+
+  // print(b ?? 0);
+  // a==null?0:null;
+  // print(a);
+
+  // List list = [1,5,2,3,3];
+  // list
+  // ..add(1)
+  // ..add(2)
+  // ..add(3)
+  // ..insert(2, 9)
+  // ..remove(2);// print(list.add(2)...)
+
+  // print(list.take(2));
+
+  // bool bl = list.any((element) => element == 1);
+  // print(bl);
+  // list.insert(1, 10);
+  // list.map((e) =>e).toList();
+  // print(list.toSet().toList());
+
+//   List list = [1,2,3];
+//   List list2 = [4,5,6];
+//   List list3 = [7,8,9];
+//   List list4 = [10,11,12];
+//   list.addAll(list2);
+//   list.addAll(list3);
+//   list.addAll(list4);
+//   print(list);
+// // list.addAll(list,list2,list3);
+//   List lists = [...list2,...list3,...list4];
+//
+
+  // List list = List.generate(10, (index) {
+  //
+  // });
+  //
+  // list.add(false);
+  // print(list);
+
+  //Type
+  // List list = List.filled(10, true,growable: true);
+  // list.add(false);
+  // print(list);
+
+  // Set a ={'maryam','mohammdi','maryam'};
+
+  // List<dynamic> list = [1,"a",true];
+  // Map<String,dynamic> map = {'name':'maryam','age': 20};
+  //----------------------------------------------------------------------------
+
+  // dynamic a = 10;
+  // double b = 10.5;
+  // // var c;
+  // a = a+b;
+  //----------------------------------------------------------------------------
+  //var
+  // int a = 10;
+  // double b = 10;
+  // String c = 'abc';
+  // bool bl = true;
+  //----------------------------------------------------------------------------
 }
